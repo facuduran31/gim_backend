f05b7d3780e2c827c20eb6dda3b2513e59df9bc6
Pedro Garrido <pedrogarrido971@gmail.com>
2024-12-09 19:38:10 -0300

    Terminé CRUD gimnasios

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const cors = require('cors');
 const express = require('express');
-const app = express();
+const gimnasioRouter = require('./routes/gimnasio');
+
+
 require('dotenv').config();
 const port = process.env.PORT;
 
+
+const app = express();
+
 //Middlewares
-app.use(cors)
+app.use(cors())
 app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send('hola')
+})
+
+app.use('/gimnasios', gimnasioRouter)
+
 
 app.listen(port, () => {
     console.log("Este es un pasito que le gusta a los turro baila pegaito a la pare olright", port)
