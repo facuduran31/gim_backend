81d4ec0f08effd8d2b9202c972e40110a9709b86
Pedro <pedrogarrido971@gmail.com>
2024-12-19 14:27:32 -0300

    gsgsg

---
+++ interfaces/usuario.js
@@ -0,0 +1,15 @@
+class Usuario {
+  id;
+  nombre;
+  apellido;
+  mail;
+  password;
+
+  constructor(id, nombre, apellido, mail, password) {
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.mail = mail;
+    this.password = password;
+  }
+}
