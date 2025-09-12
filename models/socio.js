29b377214d0b6ace98578b0b699bff65f530a14d
Pedro Garrido <pedrogarrido971@gmail.com>
2025-09-12 20:30:19 -0300

    Arranque a cambiar la inscripcion

--- models/socio.js
+++ models/socio.js
@@ -14,15 +14,15 @@ class SocioModel {
     }
 
     getSociosByGimnasio = (idGimnasio, callback) => {
-        db.query("select distinct s.idSocio, s.nombre, s.apellido, s.dni, s.telefono, s.activo FROM gimnasio g INNER JOIN plan p ON g.idGimnasio=p.idGimnasio INNER JOIN socio_plan sp ON p.idPlan=sp.idPlan INNER JOIN socio s ON sp.idSocio=s.idSocio where g.idGimnasio=?;", [idGimnasio], callback);
+        db.query("SELECT * FROM socio WHERE idGimnasio = ?", [idGimnasio], callback);
     }
 
     createSocio = (socio, callback) => {
-        db.query('INSERT INTO socio (nombre, apellido, dni, telefono, activo) VALUES (?, ?, ?, ?, ?)', [socio.nombre, socio.apellido, socio.dni, socio.telefono, socio.activo], callback);
+        db.query('INSERT INTO socio (nombre, apellido, dni, telefono, activo, idGimnasio) VALUES (?, ?, ?, ?, ?, ?)', [socio.nombre, socio.apellido, socio.dni, socio.telefono, socio.activo, socio.idGimnasio], callback);
     }
 
     updateSocio = (socio, callback) => {
-        db.query('UPDATE socio SET nombre = ?, apellido = ?, dni = ?, telefono = ?, activo = ? WHERE idSocio = ?', [socio.nombre, socio.apellido, socio.dni, socio.telefono, socio.activo, socio.id], callback);
+        db.query('UPDATE socio SET nombre = ?, apellido = ?, dni = ?, telefono = ?, activo = ?, idGimnasio = ? WHERE idSocio = ?', [socio.nombre, socio.apellido, socio.dni, socio.telefono, socio.activo, socio.id, socio.idGimnasio], callback);
     }
 
     deleteSocio = (id, callback) => {
