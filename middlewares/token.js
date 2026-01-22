4344ee8ac124382c5e6f27cb00145b8fc8e33b85
facuduran31 <facundoduran31@gmail.com>
2026-01-22 14:18:53 -0300

    Bearer added in token.js

--- middlewares/token.js
+++ middlewares/token.js
@@ -5,23 +5,25 @@ const secret = process.env.JWT_SECRET;
 
 const generateToken = (usuario) => {
   return jwt.sign(usuario, secret, { expiresIn: '1h' });
-}
-
+};
 
 const validateToken = (req, res, next) => {
+  let token = req.cookies?.authToken || req.headers['authorization'];
 
-  const token = req.cookies?.authToken || req.headers['authorization'];
-  if (token) {
-    jwt.verify(token, secret, (err, user) => {
-      if (err) {
-        return res.status(401).json({ message: 'Token no válido' });
-      } else {
-        next();
-      }
-    });
-  } else {
+  if (!token) {
     return res.status(401).json({ message: 'Token no proporcionado' });
   }
-}
+
+  if (typeof token === 'string' && token.startsWith('Bearer ')) {
+    token = token.slice('Bearer '.length).trim();
+  }
+
+  jwt.verify(token, secret, (err, user) => {
+    if (err) return res.status(401).json({ message: 'Token no válido' });
+
+    req.user = user;
+    next();
+  });
+};
 
 module.exports = { generateToken, validateToken };
